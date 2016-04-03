490cd28118d05799405ec6ed242da88a48576a16
niekas <albertas.gimbutas@mii.vu.lt>
2016-04-03 19:28:33 +0300

    Add classical functions and interface to call them

--- Asimpl_main.cpp
+++ Asimpl_main.cpp
@@ -15,8 +15,10 @@ using namespace std;
 int main(int argc, char* argv[]) {
     // Parse parameters
     const struct option longopts[] = {
-        {"func_cls", required_argument, 0, 'c'},
-        {"func_id", required_argument, 0, 'f'},
+        {"func_cls", optional_argument, 0, 'c'},
+        {"func_id", optional_argument, 0, 'f'},
+        {"func_name", optional_argument, 0, 'n'},
+        {"stop_crit", optional_argument, 0, 's'},
         {"task_id", required_argument, 0, 't'},
         {"callback", required_argument, 0, 'b'},
         {"max_duration", optional_argument, 0, 'd'},
@@ -27,6 +29,8 @@ int main(int argc, char* argv[]) {
     int fid;
     int task_id;
     char* callback = {'\0'};
+    char* func_name = {'\0'};
+    char* stop_crit = {'\0'};
     int max_calls = 40000;
     int max_duration = 3600;
     double glob_L = numeric_limits<double>::max();
@@ -34,10 +38,16 @@ int main(int argc, char* argv[]) {
     int opt_id;
     int iarg = 0;
     while(iarg != -1) {
-        iarg = getopt_long(argc, argv, "cftbdig", longopts, &opt_id);
+        iarg = getopt_long(argc, argv, "cnsftbdig", longopts, &opt_id);
         switch (iarg) {
             case 'c':
                 cls = strtoul(optarg, 0, 0);
+                break;
+            case 'n':
+                func_name = strdup(optarg);
+                break;
+            case 's':
+                stop_crit = strdup(optarg);
                 break;
             case 'f':
                 fid = strtoul(optarg, 0, 0);
@@ -60,9 +70,14 @@ int main(int argc, char* argv[]) {
         };
     };
 
-    // Put function vector in order to be able to use more than 2 functions in the future
+    // Check which function is provided and use it
     vector<Function*> funcs;
-    funcs.push_back(new GKLSFunction(cls, fid));
+    if (func_name != '\0') {
+        Function* func = get_function(func_name, stop_crit);
+        funcs.push_back(func);
+    } else {
+        funcs.push_back(new GKLSFunction(cls, fid));
+    };
 
     Asimpl* alg;
     alg = new Asimpl(max_calls, max_duration);
@@ -74,7 +89,14 @@ int main(int argc, char* argv[]) {
     alg->minimize(funcs);
 
     // Print results
-    cout << "Cls: " << cls << "  Fid: " << fid << endl;
+    double X[funcs[0]->_D];
+    for (int i=0; i < funcs[0]->_D; i++) {
+        X[i] = funcs[0]->transform_back(funcs[0]->_glob_x, i);
+    };
+    cout << "Glob value: " << funcs[0]->value(new Point(X, funcs[0]->_D)) << " == " << funcs[0]->_glob_f << endl;
+    cout.precision(12);
+    // cout << "Cls: " << cls << "  Fid: " << fid << endl;
+    cout << "Name: " << func_name << "  Stop_crit: " << stop_crit<< endl;
     if (alg->_status == "S") { cout << "  -->> Suspended <<--" << endl; }
     cout << "Calls: " << funcs[0]->_calls 
          << ", status: " << alg->_status  
@@ -83,11 +105,12 @@ int main(int argc, char* argv[]) {
 
     for (int i=0; i < funcs.size(); i++) {
         cout.precision(10);
-        cout << "Solution for criteria " << i + 1 << ":" << endl;
-        funcs[i]->_x_nearest_to_glob_x->print();
-        cout << "   Global minima for criteria " << i + 1 << ":" << endl;
+        cout << "Solution for criteria " << i + 1 << ": " << funcs[i]->_f_min << endl;
+        // funcs[i]->_x_nearest_to_glob_x->print();
+        cout << "   Global minima for criteria " << i + 1 << ": " << funcs[i]->_glob_f << endl;
         funcs[i]->_glob_x->print();
     };
+
 
     // Save results
     if (callback != '\0') {
